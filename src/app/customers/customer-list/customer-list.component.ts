e955f702dd0a5b7dc4d5c929137969279433f31c
Mike Boddin <github@autorisiert.net>
2018-05-15 21:10:11 +0200

    add customer search

--- src/app/customers/customer-list/customer-list.component.ts
+++ src/app/customers/customer-list/customer-list.component.ts
@@ -5,6 +5,9 @@ import {Observable} from 'rxjs/Observable';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import {Subscription} from 'rxjs/Subscription';
 import { Router } from '@angular/router';
+import {FormControl} from '@angular/forms';
+import {debounceTime} from 'rxjs/operators';
+import {combineLatest} from 'rxjs/observable/combineLatest';
 
 @Component({
   selector: 'app-customer-list',
@@ -16,10 +19,13 @@ export class CustomerListComponent implements OnInit, OnDestroy {
   constructor(private customerService: CustomerService, private router: Router) {}
 
   private customersSubject = new BehaviorSubject<Customer[]>([]);
+  private searchSubject = new BehaviorSubject<string>('');
 
   oldCustomers: Customer[] = [];
 
   private subscriptions: Subscription[] = [];
+
+  searchTerm = new FormControl();
 
   ngOnInit() {
     // einfach
@@ -30,6 +36,12 @@ export class CustomerListComponent implements OnInit, OnDestroy {
     const s2 = this.customerService.getAll().subscribe(customers => {
       this.customersSubject.next(customers);
     });
+    this.searchTerm.valueChanges.pipe(
+      debounceTime(250)
+    ).subscribe(text => {
+      console.log('text', text);
+      this.searchSubject.next(text);
+    });
     this.subscriptions.push(s1, s2);
   }
   ngOnDestroy(): void {
@@ -37,7 +49,10 @@ export class CustomerListComponent implements OnInit, OnDestroy {
   }
 
   get customers(): Observable<Customer[]> {
-     return this.customersSubject.asObservable();
+     // return this.customersSubject.asObservable();
+    return combineLatest(this.searchSubject, this.customersSubject, (searchText: string, customers: Customer[]) => {
+      return customers.filter(cust => !searchText || cust.name.toLowerCase().includes(searchText.toLowerCase()));
+    });
   }
 
   addNewCustomer() {

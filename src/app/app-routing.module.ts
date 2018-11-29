81074b7727dae4ab6c8324a0a94b6d547e1505b0
Mike Boddin <github@autorisiert.net>
2018-11-29 14:39:16 +0100

    add  basic routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+
+export const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: '**', component: HomeComponent}
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 // App Root
 import { AppComponent } from './app.component';
 import { CustomersModule } from './customers/customers.module';
+import {AppRoutingModule} from './app-routing.module';
+import {HomeModule} from './home/home.module';
 
 @NgModule({
   imports: [
     BrowserModule,
-    CustomersModule
+    CustomersModule,
+    AppRoutingModule,
+    HomeModule
   ],
   declarations: [
     AppComponent
